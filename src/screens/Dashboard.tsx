3c96a451ca8209d03f6d4827df6b8e9bf521d9b8
Lucas <luccaslp99@gmail.com>
2021-10-11 21:16:05 -0300

    first commit

---
+++ src/screens/Dashboard.tsx
@@ -0,0 +1,12 @@
+import react from 'react';
+import { View, Text } from 'react-native'
+
+export function Dashboard() {
+    return(
+        <View>
+            <Text>
+                
+            </Text>
+        </View>
+    )
+}
